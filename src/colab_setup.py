c67fe02b38c310d735a5d65bb7b9fe5a6dc7f0f4
Luissalinas711 <luissalinas71197@gmail.com>
2026-06-10 10:16:32 -0700

    Fix comments and improve GitHub push messages

--- src/colab_setup.py
+++ src/colab_setup.py
@@ -7,7 +7,7 @@
 
 # HOW TO USE IN YOUR OWN COLAB:
 # 1. Create a new file in Colab (title mmif-startup or mmif-finish)
-# 2. Creat a title block (##Title) and insert cell for code below.
+# 2. Creat a title block (##Title) cell and insert second cell for code below.
 # 3. Tools > Settings > Paste Url into "Custom Notebook Snippet URL" > Save
 # 4. Call the startup snippet as Cell 1 in every notebook
 # 5. Call finish snippet as the last cell before closing
@@ -32,9 +32,12 @@ REPO_PATH   = f'/content/{REPO}'   # where the repo is placed in Colab
 DATA        = f'{DRIVE_ROOT}/mmif_data'   # your image files in Google Drive
 MODELS      = f'{DRIVE_ROOT}/mmif_models'   # DenseFuse info in Google Drive
 
+# Allows src/ files to be importable by cloning Github repo
 os.system(f'git clone https://{PAT}@github.com/'Enter Your Username Here'/MMIF-SUReA2026 {REPO_PATH}')
 os.chdir(REPO_PATH)
 sys.path.insert(0, f'{REPO_PATH}/src')
+
+# Installs packages (not needed for every session)
 os.system('pip install PyWavelets scikit-image -q')
 
 print('Session ready.')
@@ -43,13 +46,19 @@ print('Session ready.')
 import subprocess
 from datetime import date
 
+# move into the repo folder
 os.chdir('/content/MMIF-SUReA2026')
 os.system('git config user.name "Enter Your Username Here"')
 os.system('git config user.email "Enter Your Username Here@users.noreply.github.com"')
+
+# readies any changed files
 os.system('git add .')
+
+# Edit here: short description of changes made this session
 subprocess.run(['git', 'commit', '-m', f"{date.today()} — EDIT THIS"])
+
+# Push file(s) to Github
 subprocess.run(['git', 'push'])
 
-
 p = subprocess.run(['git', 'push'], capture_output=True, text=True)
-print('Pushed.' if p.returncode == 0 else p.stderr)
+print('Pushed to GitHub.' if p.returncode == 0 else p.stderr)
