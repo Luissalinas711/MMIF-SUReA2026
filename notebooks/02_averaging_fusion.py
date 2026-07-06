be453c4303f329009b1967fbacc287df202cc835
Luissalinas711 <luissalinas71197@gmail.com>
2026-07-05 22:02:17 -0700

    Update comments for consistency in case formatting

--- notebooks/02_averaging_fusion.py
+++ notebooks/02_averaging_fusion.py
@@ -1,6 +1,6 @@
 # Method 1: Averaging Fusion.
-# Remember to run the mmif-startup snippet first (it sets DATA, REPO_PATH and puts src/ on the path). 
-# Then run this cell and mmif-finish at the end.
+# Remember to run the MMIF Startup snippet first (it sets DATA, REPO_PATH and puts src/ on the path). 
+# Then run this cell and MMIF Finish at the end.
 
 from fusion.averaging import fuse_average
 from pipeline import fuse_all_pairs
@@ -12,5 +12,5 @@ fuse_all_pairs(
     data_root=DATA,
     results_root=f'{REPO_PATH}/results',
     label='Average Fused',
-    show=True,          # set False if I just want it to save w/out showing every figure
+    show=True,          # set False to save w/out showing every figure
 )
